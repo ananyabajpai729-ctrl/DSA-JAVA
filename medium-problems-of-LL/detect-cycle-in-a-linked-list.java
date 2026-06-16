237adaefc8870d7a5640c1185055a2c585e113a6
Ananya Bajpai <ananya.bajpai729@gmail.com>
2026-06-16 18:09:49 +0530

    Implement cycle detection in a linked list

---
+++ medium-problems-of-LL/detect-cycle-in-a-linked-list.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode turtle = head;
+        ListNode hare = head;
+
+        while(hare != null && hare.next != null){
+            turtle = turtle.next;
+            hare = hare.next.next;
+            if(turtle == hare)return true;
+        }
+        return false;
+    }
+}
