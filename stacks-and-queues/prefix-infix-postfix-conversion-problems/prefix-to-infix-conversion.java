f01626819ea7475af72aac4a265c756819ef5f86
Ananya Bajpai <ananya.bajpai729@gmail.com>
2026-07-06 18:29:35 +0530

    Implement prefix to infix conversion method

---
+++ stacks-and-queues/prefix-infix-postfix-conversion-problems/prefix-to-infix-conversion.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String prefixToInfix(String s) {
+        Stack<String> stk = new Stack<>();
+        int n = s.length();
+
+        for(int i = n - 1; i>= 0; i--){
+            char c = s.charAt(i);
+
+            if(Character.isLetterOrDigit(c)){
+                stk.push(String.valueOf(c));
+            }else{
+                String op1 = stk.pop();
+                String op2 = stk.pop();
+                
+                stk.push("(" + op1 + c + op2 + ")");
+            }
+        }
+
+        return stk.peek();
+    }
+}
