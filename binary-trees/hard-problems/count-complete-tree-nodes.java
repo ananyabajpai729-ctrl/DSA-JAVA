9737ee9c29068ae7e0f3f7db8dfc70f903b3862b
Ananya Bajpai <ananya.bajpai729@gmail.com>
2026-08-05 20:04:46 +0530

    Add countNodes method to count tree nodes

---
+++ binary-trees/hard-problems/count-complete-tree-nodes.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int countNodes(TreeNode root) {
+        if(root == null) return 0;
+
+        int lh = findLeftHeight(root);
+        int rh = findRightHeight(root);
+
+        if(lh == rh) return (1 << lh) - 1;
+
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+    private int findLeftHeight(TreeNode root){
+        int height = 0;
+        while(root != null){
+            height++;
+            root = root.left;
+        }
+        return height;
+    }
+    private int findRightHeight(TreeNode root){
+        int height = 0;
+        while(root != null){
+            height++;
+            root = root.right;
+        }
+        return height;
+    }
+}
