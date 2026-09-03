a82634dffa0f37184e715cfa3a38c778fbbead23
Ananya Bajpai <ananya.bajpai729@gmail.com>
2026-09-03 13:03:12 +0530

    Implement BFS for topological sorting

---
+++ graphs/topo-sort/topo-sort-bfs.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[] topoSort(int V, List<List<Integer>> adj) {
+        int[] ans = new int[V];
+
+        int[] indegree = new int[V];
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for(int i = 0; i <V; i++){
+            for(int it: adj.get(i)){
+                indegree[it]++;
+            }
+        }
+
+        for(int i = 0; i <V; i++){
+            if(indegree[i] == 0){
+                q.add(i);
+            }
+        }
+        int index = 0; 
+        while(!q.isEmpty()){
+            ans[index] = q.peek();
+            index++;
+            int node = q.poll();
+
+            for(int it: adj.get(node)){
+                indegree[it]--;
+                if(indegree[it]== 0){
+                    q.add(it);
+                }
+            }
+        }
+
+        return ans;
+    }
+}
